ba69f458ed67ae24018540e9c0c3f8bb198e3453
Denys Vuika <denys.vuika@gmail.com>
2019-07-10 20:44:19 +0100

    update ci configs

--- src/app/translate-lazy-demo/demo-page/demo-page.component.ts
+++ src/app/translate-lazy-demo/demo-page/demo-page.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'ngs-demo-page',
   templateUrl: './demo-page.component.html',
   styleUrls: ['./demo-page.component.css']
 })
 export class DemoPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
